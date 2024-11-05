ee29b664dcec39dcd039523bb35480a9b1b3cd12
Nacho <ignacio.mattano199199@alumnos.info.unlp.edu.ar>
2024-11-05 00:18:30 -0300

    Info fix

--- src/components/Home/Info.tsx
+++ src/components/Home/Info.tsx
@@ -7,16 +7,16 @@ export default function Info() {
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
                     <div className="flex flex-col justify-center p-12 space-y-8">
                     <h2 className="text-3xl font-bold">
-                        La propiedad de tus sueños ahora tokenizada
+                        Inverti en nuestros autos en simples pasos
                     </h2>
                     <p>
-                        Explora un nuevo horizonte en el mercado inmobiliario con nuestra innovadora plataforma de tokenización. Convierte tu visión de la propiedad ideal en una realidad tangible y accesible.
+                        Explora un nuevo horizonte en el mercado automotriz con nuestra innovadora plataforma de tokenización. Convierte tu visión del automóvil ideal en una realidad tangible y accesible.
                     </p>
                     </div>
                     <div className="flex justify-center">
                     <img
                         src={RENT.src}
-                        alt="Resttfy"
+                        alt="ChainCars"
                         className="rounded-md"
                     />
                     </div>
